fd9a1dc11d0ebc25f1d076a82f9a143abfc3dd72
thiennguyen96841996 <thiennguyen96841996@gmail.com>
2018-11-28 19:03:01 +0700

    Create multiauth

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,6 +60,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'admin.auth' => \App\Http\Middleware\AdminRedirectIfNotAuthenticated::class,
+        'admin.guest' => \App\Http\Middleware\AdminRedirectIfAuthenticated::class,
     ];
 
     /**
