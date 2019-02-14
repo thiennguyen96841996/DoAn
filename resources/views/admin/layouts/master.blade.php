ea28fa375c17f81e2871c8493c1f4d60b6ee8487
thiennguyen <thiennguyen96841996@gmail.com>
2019-02-14 10:10:07 +0700

    Create layout

---
+++ resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>@yield('title')</title>
+
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" href="{{ asset('assets/images/logo/apple-touch-icon.html') }}">
+    <link rel="shortcut icon" href="{{ asset('assets/images/logo/favicon.png') }}">
+
+    <!-- core dependcies css -->
+    <link rel="stylesheet" href="{{ asset('assets/vendor/bootstrap/dist/css/bootstrap.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendor/PACE/themes/blue/pace-theme-minimal.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendor/perfect-scrollbar/css/perfect-scrollbar.min.css') }}" />
+
+    <!-- page css -->
+    @yield('CssPage')
+
+    <!-- core css -->
+    <link href="{{ asset('assets/css/font-awesome.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/themify-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/materialdesignicons.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/animate.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <div class="app header-success-gradient">
+        <div class="layout">
+            <!-- Header START -->
+            @include('admin.layouts.header')
+            <!-- Header END -->
+
+            <!-- Side Nav START -->
+            @include('admin.layouts.navbar')
+            <!-- Side Nav END -->
+
+            <!-- Page Container START -->
+            <div class="page-container">
+                <!-- Quick View START -->
+                @include('admin.layouts.quickview')
+                <!-- Side Panel END -->
+
+                <!-- Content Wrapper START -->
+                @yield('content')
+                <!-- Content Wrapper END -->
+
+                <!-- Footer START -->
+                <footer class="content-footer">
+                    <div class="footer">
+                        <div class="copyright">
+                            <span>Copyright © <b class="text-dark"></b>tientien</span>
+                            <span class="go-right">
+                                <a href="#" class="text-gray m-r-15">Term &amp; Conditions</a>
+                                <a href="#" class="text-gray">Privacy &amp; Policy</a>
+                            </span>
+                        </div>
+                    </div>
+                </footer>
+                <!-- Footer END -->
+            </div>
+            <!-- Page Container END -->
+        </div>
+    </div>
+    <script src="{{ asset('assets/js/vendor.js') }}"></script>
+
+    <script src="{{ asset('assets/js/app.min.js') }}"></script>
+
+    <!-- page js -->
+    @yield('JsPage')
+    
+</body>
+</html>
