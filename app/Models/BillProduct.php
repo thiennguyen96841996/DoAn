bca1f7a84897e9489e8e33fe5342310bebcf5493
thiennguyen <thiennguyen96841996@gmail.com>
2019-02-14 10:28:23 +0700

    Create models

---
+++ app/Models/BillProduct.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class BillProduct extends Model
+{
+    use SoftDeletes;
+}
