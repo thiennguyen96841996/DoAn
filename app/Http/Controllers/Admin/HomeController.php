c5eee6cf4a98c10d6c30cf1a4627cf77fea30a93
thiennguyen <thiennguyen96841996@gmail.com>
2019-04-22 15:21:04 +0700

    Fix layout admin home page

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Booking;
+use App\Models\BillTable;
+use Carbon\Carbon;
 
 class HomeController extends Controller
 {
@@ -23,6 +26,25 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('admin.home');
+        $bill_count = BillTable::where('status', 0)
+        ->whereDay('updated_at', Carbon::now()->format('d'))
+        ->whereMonth('updated_at', Carbon::now()->format('m'))
+        ->whereYear('updated_at', Carbon::now()->format('Y'))
+        ->selectRaw('sum(paymented) as sum, count(id) as count')
+        ->get();
+        $bill_detail_count = BillTable::where('status', 1)
+        ->whereDay('updated_at', Carbon::now()->format('d'))
+        ->whereMonth('updated_at', Carbon::now()->format('m'))
+        ->whereYear('updated_at', Carbon::now()->format('Y'))
+        ->selectRaw('sum(paymented) as sum, count(id) as count')
+        ->get();
+        $customer_count = BillTable::groupBy('customer_id')
+        ->selectRaw('customer_id')
+        ->whereDay('updated_at', Carbon::now()->format('d'))
+        ->whereMonth('updated_at', Carbon::now()->format('m'))
+        ->whereYear('updated_at', Carbon::now()->format('Y'))
+        ->get()
+        ->count('customer_id');
+        return view('admin.home', compact('bill_count', 'bill_detail_count', 'customer_count'));
     }
 }
